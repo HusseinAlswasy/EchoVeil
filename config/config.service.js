3a260fb5de20c0b267053be21c7c58c7c48f300e
Hussein Hassan <01096694296hud@gmail.com>
2026-09-24 02:39:06 +0400

    update config services

--- config/config.service.js
+++ config/config.service.js
@@ -1,24 +1,33 @@
 import dotenv from "dotenv";
 import { resolve } from "node:path";
 
-const node_env = process.env.NODE_ENV;
+const node_env = process.env.NODE_ENV || "development";
 
-let env_pathes = {
+const env_paths = {
     development: ".env.development",
-    production: ".env.production"
+    production: ".env.production",
+};
+
+if (node_env === "development") {
+    dotenv.config({
+        path: resolve(`config/${env_paths[node_env]}`)
+    });
 }
 
-dotenv.config({ path: resolve(`config/${env_pathes[node_env]}`) })
+if (node_env === "production") {
+    dotenv.config({
+        path: resolve(`config/${env_paths[node_env]}`)
+    });
+}
 
-
-export const PORT = process.env.PORT
-export const MONGO_URI = process.env.MONGO_URI
-export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY
-export const JWT_SECRET = process.env.JWT_SECRET
-export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET
-export const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME
-export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY
-export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET
-export const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS
-export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
-export const REDIS_URL = process.env.REDIS_URL
+export const PORT = process.env.PORT;
+export const MONGO_URI = process.env.MONGO_URI;
+export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+export const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+export const REDIS_URL = process.env.REDIS_URL;
